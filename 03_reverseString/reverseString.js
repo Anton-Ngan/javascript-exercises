b957c1714b7de910dfad2e2ef193b1a321e89156
Anton Ngan <anton.an.ngan@gmail.com>
2025-03-14 12:29:43 +1100

    Complete all js exercises from 1 to 7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = "";
+    let stringArr = string.split("");
+    for (i = stringArr.length - 1; i >= 0; i--) {
+        newString = newString.concat(stringArr[i]);
+    }
+    return newString
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(op1, op2) {
+    if (op1 < 0 
+        || op2 < 0 
+        || !Number.isInteger(op1) 
+        || !Number.isInteger(op2)) {
+        return "ERROR";
+    }
 
+    lowerBound = Math.min(op1, op2);
+    upperBound = Math.max(op1, op2);
+
+    let totalSum = 0;
+
+    for (i=lowerBound; i<=upperBound; i++) {
+        totalSum += i;
+    }
+
+    return totalSum;
 };
 
 // Do not edit below this line
